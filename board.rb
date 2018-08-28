ef62e1505ce7c4036846c014294caf82e7202012
jeremiahkellick <jeremiah@jeremiahkellick.com>
2018-08-28 15:04:41 -0700

    Switch to real pices in Board

--- board.rb
+++ board.rb
@@ -1,4 +1,7 @@
 require_relative "piece"
+require_relative "slideable"
+require_relative "stepable"
+require_relative "pawn"
 
 # Board.make_board => Board.new
 
@@ -13,13 +16,29 @@ class Board
 
 
   def initialize
-    @grid = Array.new(8) {Array.new(8, NullPiece.instance) }
-    fill_board
+    row = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
+    @kings = {}
+    black_rows, white_rows = [:black, :white].map do |color|
+      royal_row = row.map do |piece_class|
+        new_piece = piece_class.new(nil, self, color)
+        @kings[color] = new_piece if piece_class == King
+        new_piece
+      end
+      pawn_row = 8.times.map { Pawn.new(nil, self, color) }
+      color == :black ? [royal_row, pawn_row] : [pawn_row, royal_row]
+    end
+    middle = Array.new(4) { Array.new(8, NullPiece.instance) }
+    @grid = black_rows + middle + white_rows
+    fix_piece_positions
+    @last_move = nil
+    @destroyed_piece = nil
   end
 
-  def fill_board
-    (@grid[0..1] + @grid[-2..-1]).each_with_index do |row, row_i|
-      row.each_index {|i| row[i] = Piece.new([row_i, i], self, :white) }
+  def fix_piece_positions
+    @grid.each_with_index do |row, row_i|
+      row.each_with_index do |piece, col_i|
+        piece.pos = [row_i, col_i] unless piece.null?
+      end
     end
   end
 
@@ -31,7 +50,9 @@ class Board
     piece = self[start_pos]
     raise ArgumentError, "there is no piece at start_pos" if piece.nil?
     raise InvalidMoveError unless move_valid?(start_pos, end_pos)
-    self[start_pos] = nil
+    self[start_pos] = NullPiece.instance
+    @last_move = [start_pos, end_pos]
+    @destroyed_piece = self[end_pos]
     self[end_pos] = piece
     piece.pos = end_pos
   end
@@ -43,7 +64,35 @@ class Board
   def valid_pos?(pos)
     pos.all? { |coord| coord >= 0 && coord <= 7 }
   end
-  
+
+  def in_check?(color)
+    king_pos = @kings[color].pos
+    other_team_color = color == :white ? :black : :white
+    pieces_for(other_team_color).any? { |piece| piece.moves.include?(king_pos) }
+  end
+
+  def checkmate?(color)
+    in_check?(color) && pieces_for(color).all? do |piece|
+      piece.valid_moves.empty?
+    end
+  end
+
+  def pieces_for(color)
+    result = []
+    @grid.each do |row|
+      row.each { |piece| result << piece if piece.color == color }
+    end
+    result
+  end
+
+  def undo
+    moved_piece = self[@last_move[1]]
+    self[@last_move[0]] = moved_piece
+    moved_piece.pos = @last_move[0]
+    self[@last_move[1]] = @destroyed_piece
+    @destroyed_piece.pos = @last_move[1]
+  end
+
   private
 
   def []=(pos, value)
