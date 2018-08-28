94689b3541b1d802525a3b6deebc282fafc926fa
jeremiahkellick <jeremiah@jeremiahkellick.com>
2018-08-28 16:19:51 -0700

    Allow pawns to move twice on first move

--- pawn.rb
+++ pawn.rb
@@ -4,13 +4,23 @@ class Pawn < Piece
   def initialize(pos, board, color)
     super
     @vertical = color == :white ? -1 : 1
+    @first_time_pos_set = true
+    @starting_row = nil
+  end
+
+  def pos=(value)
+    super
+    if @first_time_pos_set
+      @starting_row = value[0]
+      @first_time_pos_set = false
+    end
   end
 
   def moves
     result = []
-    straight_pos = add_pos(@pos, straight_diff)
-    result << straight_pos if @board.valid_pos?(straight_pos) &&
-                              @board[straight_pos].null?
+    straight_positions.each do |pos|
+      result << pos if @board.valid_pos?(pos) && @board[pos].null?
+    end
     diagonal_diffs.each do |diff|
       new_pos = add_pos(@pos, diff)
       if @board.valid_pos?(new_pos) &&
@@ -29,6 +39,14 @@ class Pawn < Piece
 
   def straight_diff
     [@vertical, 0]
+  end
+
+  def straight_positions
+    if @pos[0] == @starting_row
+      [add_pos(@pos, straight_diff), add_pos(@pos, [@vertical * 2, 0])]
+    else
+      [add_pos(@pos, straight_diff)]
+    end
   end
 
   def to_s
