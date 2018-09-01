1ba05e76ed4f2af2f59c202f11d409ee571a89ee
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2018-09-01 10:57:18 -0700

    Change grid size to 2x1

--- display.rb
+++ display.rb
@@ -15,16 +15,16 @@ class Display
   def render
     string = ""
     black = false
-    (8 * 3).times do |i|
-      black = !black if i % 3 == 0
-      (8 * 6).times do |j|
-        pos = [i / 3, j / 6]
+    (8).times do |i|
+      black = !black
+      (8 * 2).times do |j|
+        pos = [i, j / 2]
         piece = @board[pos]
-        black = !black if j % 6 == 0
+        black = !black if j % 2 == 0
         bg = black ? :black : :light_black
         bg = :blue if @highlighted.include?(pos)
         bg = :red if pos == @cursor.cursor_pos
-        if (i - 1) % 3 == 0 && (j - 2) % 6 == 0
+        if j % 2 == 0
           color = piece.color == :white ? :white : :magenta
           string << piece.to_s.colorize(color: color, background: bg)
         else
